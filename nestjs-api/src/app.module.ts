ed272d42b23d84f0c8a92c8d07081d2149118dfb
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-03 15:45:40 -0400

    feat[nestjs-api]: Criando aplicação nest

---
+++ nestjs-api/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Product } from './products/entities/product.entity';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+      logging: true,
+    }),
+    ProductsModule,
+    OrdersModule,
+  ],
+  controllers: [AppController], //MVC  Model View Controller
+  providers: [AppService],
+})
+export class AppModule {}
