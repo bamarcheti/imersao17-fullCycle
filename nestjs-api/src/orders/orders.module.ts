415b1984b0e83d631b8a32e0d615617dc7287669
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-04 10:38:11 -0400

    feat[nestjs-api]: Adicionado mensagens de pagamento pelo RabbitMQ

--- nestjs-api/src/orders/orders.module.ts
+++ nestjs-api/src/orders/orders.module.ts
@@ -3,13 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../products/entities/product.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
+import { OrderConsumer } from './order.consumer';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-// import { OrderConsumer } from './order.consumer';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderConsumer],
 })
 export class OrdersModule {}
