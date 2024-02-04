62a5eaf5288487c569b5942b9128373a774343f4
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-03 21:22:23 -0400

    feat[nextjs-frontend]: Criando site de pedidos

---
+++ nextjs-frontend/src/app/layout.tsx
@@ -0,0 +1,44 @@
+import { Box, ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Navbar } from "../components/Navbar/Navbar";
+import theme from "../theme";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Code Commerce",
+  description: "Uma Full Cycle shop",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <div>
+              <Navbar />
+              <Box
+                component="main"
+                sx={{
+                  flexGrow: 1,
+                  bgcolor: "background.default",
+                  mt: ["122px", "135px", "146px"],
+                  p: 3,
+                }}
+              >
+                {children}
+              </Box>
+            </div>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
